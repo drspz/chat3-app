b109626296b3637ddd26a55e7efb4fc7ff677640
drspz <spz0707k@gmail.com>
2020-08-02 09:28:40 +0900

    投稿時刻の日本時間への修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,8 +5,9 @@ class MessagesController < ApplicationController
     @messages = @room.messages.includes(:user)
   end
 
+  
   def create
-    @room = Room.find([:room_id])
+    @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room)
@@ -17,9 +18,11 @@ class MessagesController < ApplicationController
 
   private
 
-  def message_params
-     params.require(:message).permit(:content).merge(user_id: current_user.id) 
-  end
+ def message_params
+   params.require(:message).permit(:content).merge(user_id: current_user.id)
+ end
+
+  
 end
 
  
